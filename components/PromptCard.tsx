44354b157c463eb1a4268cc090c2221d0224c6f6
MmmarRTHa <m27842n@outlook.com>
2023-10-02 22:18:24 -0700

    - update: feed-feature - added profile cards and edit / delete UI.

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+    const { data: session } = useSession();
+    const pathName = usePathname();
+    const router = useRouter();
     const [copied, setCopied] = useState("");
 
     const handleCopy = () => {
@@ -40,7 +45,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
             <p className="font-inter text-sm blue_gradient cursor-pointer"
                 onClick={() => handleTagClick && handleTagClick(post.tag)}
-            >{post.tag}</p>
+            >{post.tag}
+            </p>
+            {session?.user.id === post.creator._id && pathName === "/profile" && (
+                <div className="mt-5 flex-center gap-4 border-t border-gray-200 pt-3">
+                    <p className=" font-inter text-sm green_gradient cursor-pointer"
+                    onClick={handleEdit}>Edit
+                    </p>
+                    <p className=" font-inter text-sm orange_gradient cursor-pointer"
+                    onClick={handleDelete}>Delete
+                    </p>
+                </div>
+            )}
         </div>
     );
 };
