ca6cc582227e45655b70cc91c1c2d9fda97cdd56
MmmarRTHa <m27842n@outlook.com>
2024-04-03 22:34:03 -0700

    - update: main - fix deploy issues.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
         serverComponentsExternalPackages: ["mongoose"],
+        missingSuspenseWithCSRBailout: false,
     },
     images: {
         domains: ['lh3.googleusercontent.com'],
